586896612cdf93cb7a42c69d8205e78d2452f6a1
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-24 22:32:06 +0200

    - [X] Generate a TOC at point

--- markdown-toc.el
+++ markdown-toc.el
@@ -13,31 +13,26 @@
 
 ;;; Code:
 
+(require 's)
+(require 'dash)
 (require 'markdown-mode)
 
 ;; dev
 ;; (trace-function 'markdown-imenu-create-index)
 ;; (untrace-function 'markdown-imenu-create-index)
 
-(defun markdown-toc/--compute-toc (imenu-index)
+(defun markdown-toc/--compute-toc-structure-from-level (level menu-index)
+  "Given a LEVEL and a MENU-INDEX, compute the toc structure."
+  (when menu-index
+    (let* ((fst   (car menu-index))
+           (tail  (cdr menu-index))
+           (ttail (if (integerp tail) nil (cdr tail))))
+      (cons `(,level . ,fst)
+            (--mapcat (markdown-toc/--compute-toc-structure-from-level (+ 1 level) it) ttail)))))
+
+(defun markdown-toc/--compute-toc-structure (imenu-index)
   "Given a IMENU-INDEX, compute the TOC structure."
-  (let ((fst  (car imenu-index))
-        (tail (cdr imenu-index)))
-    (cond ((integerp (cdr fst)) (cons (car fst) (markdown-toc/--compute-toc tail)))
-    )))
-
-;; Initial parsing
-'(("some markdown page title" . 1)
-  ("main title"
-  (#1="." . 52)
-  ("Sources" (#1# . 130) ("Marmalade (recommended)" . 311) ("Melpa-stable" . 552) ("Melpa (~snapshot)" . 792))
-  ("Install" (#1# . 1184) ("Load org-trello" . 1277) ("Alternative" (#1# . 1563) ("Git" . 1580) ("Tar" . 1881))))
-
- ("another title"
-  (#1# . 2044)
-  ("with" . 2061)
-  ("some" . 2070)
-  ("heading" . 2079)))
+  (--mapcat (markdown-toc/--compute-toc-structure-from-level 0 it) imenu-index))
 
 (defun markdown-toc/--symbol (sym n)
   "Compute the repetition of a symbol SYM N times as a string."
@@ -45,22 +40,25 @@
     (-repeat it sym)
     (s-join "" it)))
 
-(defun markdown-toc/--to-markdown-toc (level-title-toc-list)
-  "Given LEVEL-TITLE-TOC-LIST, a list of pair level, title, return a TOC string."
-  (->> level-title-toc-list
-    (--map (let ((nb-spaces (* 8 (car it)))
-                 (title     (cdr it)))
-             (format "%s- %s" (markdown-toc/--symbol " " nb-spaces) (markdown-toc/--to-link title))))
-    (s-join "\n")))
-
 (defun markdown-toc/--to-link (title)
   "Given a TITLE, return the markdown link associated."
   (format "[%s](#%s)" title (replace-regexp-in-string " " "-" (downcase title))))
 
+(defun markdown-toc/--to-markdown-toc (level-title-toc-list)
+  "Given LEVEL-TITLE-TOC-LIST, a list of pair level, title, return a TOC string."
+  (->> level-title-toc-list
+    (--map (let ((nb-spaces (* 4 (car it)))
+                 (title     (cdr it)))
+             (format "%s- %s" (markdown-toc/--symbol " " nb-spaces) (markdown-toc/--to-link title))))
+    (s-join "\n")))
+
 (defun markdown-toc/generate-toc ()
   "Called from within a markdown file, this will generate a TOC at current position."
   (interactive)
-  (markdown-imenu-create-index))
+  (-> (markdown-imenu-create-index)
+    markdown-toc/--compute-toc-structure
+    markdown-toc/--to-markdown-toc
+    insert))
 
 (provide 'markdown-toc)
 ;;; markdown-toc.el ends here
