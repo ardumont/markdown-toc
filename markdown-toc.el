9565eeaa1d26bc0ab83eb65bd30470888f724044
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2020-05-17 14:33:00 +0200

    Release 0.1.5

--- markdown-toc.el
+++ markdown-toc.el
@@ -5,7 +5,7 @@
 ;; Maintainer: Antoine R. Dumont (@ardumont)
 ;; URL: http://github.com/ardumont/markdown-toc
 ;; Created: 24th May 2014
-;; Version: 0.1.4
+;; Version: 0.1.5
 ;; Keywords: markdown, toc, tools,
 ;; Package-Requires: ((markdown-mode "2.1") (dash "2.11.0") (s "1.9.0"))
 
@@ -62,7 +62,7 @@
 (require 'dash)
 (require 'markdown-mode)
 
-(defconst markdown-toc--toc-version "0.1.4" "Current version installed.")
+(defconst markdown-toc--toc-version "0.1.5" "Current version installed.")
 
 (defgroup markdown-toc nil
   "A simple TOC generator for markdown file."
