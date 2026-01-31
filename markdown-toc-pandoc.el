1dbdafffff7ea3cff3c60a4949c9cbf286e70e87
JenChieh <jcs090218@gmail.com>
2026-01-31 22:39:33 +0800

    fix: Add missing header and file info

--- markdown-toc-pandoc.el
+++ markdown-toc-pandoc.el
@@ -1,5 +1,32 @@
 ;;; markdown-toc-pandoc.el --- Generates TOC compatible with pandoc -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2026 SZTK
+
+;; Author: SZTK (@Trichiurus-lepturus)
+;; Maintainer: Jen-Chieh Shen <jcs090218@gmail.com>
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING. If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; The pandoc markdown TOC generator implementation.
+;;
+
 ;;; Code:
 
 (require 's)
